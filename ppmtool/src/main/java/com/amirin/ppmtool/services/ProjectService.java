fb68608184523e19a9c3fa4ac9e58190559bd6a3
amirin <amirinsamsudin@gmail.com>
2019-08-03 20:12:37 +0800

    Custom Exceptions for Unique Project Identifiers

--- ppmtool/src/main/java/com/amirin/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/amirin/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.amirin.ppmtool.services;
 
 import com.amirin.ppmtool.domain.Project;
+import com.amirin.ppmtool.exceptions.ProjectIdException;
 import com.amirin.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return  projectRepository.save(project);
+
+        try{
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw  new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase() + " already exists");
+        }
     }
 }
