340e959c75251cf4f591f957b0ed5be675623fc9
amirin <amirinsamsudin@gmail.com>
2019-08-03 18:46:42 +0800

    Set up project object validation

--- ppmtool/src/main/java/com/amirin/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/amirin/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package com.amirin.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -9,12 +13,28 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Project name is required")
     private String projectName;
+
+    @NotBlank(message = "Project identifier is required")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+
+    @NotBlank(message = "Project description is required")
     private  String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date end_date;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     public Project() {
@@ -58,6 +78,14 @@ public class Project {
 
     public void setStart_date(Date start_date) {
         this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
     }
 
     public Date getCreated_At() {
