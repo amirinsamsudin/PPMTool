29cccb14115c5926e8bd689c33fea3bd6c47d4ef
amirin <amirinsamsudin@gmail.com>
2019-08-03 19:53:40 +0800

    Refactor Project Controller

--- ppmtool/src/main/java/com/amirin/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/amirin/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.amirin.ppmtool.web;
 
 import com.amirin.ppmtool.domain.Project;
+import com.amirin.ppmtool.services.MapValidationErrorService;
 import com.amirin.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,17 +25,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if(result.hasErrors()){
-
-            Map<String, String> errorMap = new HashMap<>();
-            for(FieldError error : result.getFieldErrors()){
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null) return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
